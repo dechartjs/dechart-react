63b71f9b8394812d2a9b54f5eb82440d07caf774
eldeni <eldeniyenden@gmail.com>
2018-11-06 17:06:48 +0900

    chore: setup examples under `examples`/
    - `favicon.ico` is created.
    - WebpackDevServer and the small configuration is used to launch the example.
    - `babelrc` is moved to `babel/`.
    - index.html is created to be used by HtmlWebpackPlugin.
    
    Fixes https://github.com/dechartjs/dechart-react/issues/3

--- internals/gulp/gulpfile.js
+++ internals/gulp/gulpfile.js
@@ -1,6 +1,6 @@
 (function changeCurrentWorkingDirectoryToResolveNodeModulesPath() {
   process.chdir('../../');
-  console.info('Current working directory %s', process.cwd());
+  console.info('[gulpfile] Current working directory %s', process.cwd());
 })();
 
 const babel = require('gulp-babel');
@@ -9,8 +9,10 @@ const del = require('del');
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const util = require('util');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
 
-const babelRc = require('./.babelrc');
+const babelRc = require('../babel/.babelrc');
 const paths = require('../../src/paths');
 
 const buildLog = (tag, ...args) => {
@@ -21,6 +23,7 @@ const Task = {
   BABEL: 'babel',
   BUILD: 'build',
   CLEAN: 'clean',
+  EXAMPLE: 'example',
 };
 
 gulp.task(Task.BABEL, () => {
@@ -45,6 +48,29 @@ gulp.task(Task.CLEAN, () => {
   return del([
     `${paths.output}/**/*`,
   ]);
+});
+
+gulp.task(Task.EXAMPLE, (done) => {
+  const webpackConfig = require('../webpack/webpack.example.config');
+  buildLog(
+    Task.EXAMPLE, 
+    'EXAMPLE_PATH: %s,\nwebpackConfig: %o', 
+    './examples', 
+    webpackConfig,
+  );
+
+  const compiler = webpack(webpackConfig);
+  new WebpackDevServer(compiler, {
+    contentBase: paths.public,
+    hot: true,
+  })
+    .listen(6001, 'localhost', (err) => {
+      console.log('[webpack] contentBase: %s', paths.public);
+      
+      if (err) {
+        console.error(err);
+      }
+    });
 });
 
 gulp.task(Task.BUILD, gulp.series(Task.CLEAN, Task.BABEL));
